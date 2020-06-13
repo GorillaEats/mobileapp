47548113981b2b5a561b7f263bde54640f4e1f8b
Sarah Ali <asarahali00@gmail.com>
2020-06-13 15:14:36 -0500

    remove markers from google maps

--- lib/widgets/googlemaps.dart
+++ lib/widgets/googlemaps.dart
@@ -22,23 +22,12 @@ class _GoogleMapsState extends State<GoogleMaps> {
     });
   }
 
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    setState(() {
-      final marker = Marker(
-        markerId: MarkerId('initial position'),
-        position: _initialPosition,
-      );
-      _markers[marker.markerId.toString()] = (marker);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _initialPosition == null
           ? LoadingScreen()
           : GoogleMap(
-              onMapCreated: _onMapCreated,
               myLocationEnabled: true,
               initialCameraPosition: CameraPosition(
                 target: _initialPosition,
