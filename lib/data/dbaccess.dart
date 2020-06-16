65e71d6000b26f503a6dab5d7357ed61c094a041
Sarah Ali <asarahali00@gmail.com>
2020-06-15 19:32:06 -0500

    make connection string const

--- lib/data/dbaccess.dart
+++ lib/data/dbaccess.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+const connection = 'lib/data/starbucks.json';
+
 Future<dynamic> getDBLocations(LatLng position) async {
-  final jsonString = await rootBundle.loadString('lib/data/starbucks.json');
+  final jsonString = await rootBundle.loadString(connection);
   final dynamic decoded = json.decode(jsonString);
   return decoded;
 }
