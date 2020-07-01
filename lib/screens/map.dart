25d814a5a7f53ca53dfb09cec97f21f8e1fa6347
Kevo1ution <kevin26428@gmail.com>
2020-07-01 00:39:54 -0500

    fixed search box not unfocusing

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:gorilla_eats/widgets/search/search.dart';
+import 'package:flutter_focus_watcher/flutter_focus_watcher.dart';
 import 'package:gorilla_eats/widgets/googlemaps.dart';
 
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body: Stack(
-        children: [
-          GoogleMaps(),
-          SafeArea(
-            child: Search(),
-          )
-        ],
+    return FocusWatcher(
+      child: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Stack(
+          children: [
+            GoogleMaps(),
+            SafeArea(
+              child: Search(),
+            )
+          ],
+        ),
       ),
     );
   }
