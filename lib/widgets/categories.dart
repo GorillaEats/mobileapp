f3b0ecf890f28490c3714a963e7ea1410af52f11
Sarah Ali <asarahali00@gmail.com>
2020-07-01 22:27:08 -0500

    move functions in categories.dart into CategoriesState

--- lib/widgets/categories.dart
+++ lib/widgets/categories.dart
@@ -3,53 +3,6 @@ import 'package:gorilla_eats/data/fooditems.dart';
 import 'package:gorilla_eats/screens/loading.dart';
 import 'package:gorilla_eats/widgets/foodcard.dart';
 
-Widget categoryHeader(String categoryName) {
-  return Column(
-    children: <Widget>[
-      Align(
-        alignment: Alignment.centerLeft,
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-          child: Text(
-            categoryName,
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              fontSize: 17,
-            ),
-          ),
-        ),
-      ),
-      Container(
-        padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
-        width: 350,
-        child: Divider(
-          color: Colors.black,
-          height: 30,
-          thickness: 2,
-        ),
-      ),
-    ],
-  );
-}
-
-Widget createCategory(dynamic category) {
-  final categoryName = category[0].category.toString();
-  final foodItems = category[1] as List<FoodItem>;
-
-  return Column(
-    children: <Widget>[
-      categoryHeader(categoryName),
-      Column(
-        children: foodItems.map((foodItem) {
-          return FoodCard(
-            foodItem: foodItem,
-          );
-        }).toList(),
-      ),
-    ],
-  );
-}
-
 class Categories extends StatefulWidget {
   @override
   _CategoriesState createState() => _CategoriesState();
@@ -66,6 +19,53 @@ class _CategoriesState extends State<Categories> {
         _allCategories = value;
       });
     });
+  }
+
+  Widget categoryHeader(String categoryName) {
+    return Column(
+      children: <Widget>[
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+            child: Text(
+              categoryName,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 17,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          width: 350,
+          child: Divider(
+            color: Colors.black,
+            height: 30,
+            thickness: 2,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget createCategory(dynamic category) {
+    final categoryName = category[0].category.toString();
+    final foodItems = category[1] as List<FoodItem>;
+
+    return Column(
+      children: <Widget>[
+        categoryHeader(categoryName),
+        Column(
+          children: foodItems.map((foodItem) {
+            return FoodCard(
+              foodItem: foodItem,
+            );
+          }).toList(),
+        ),
+      ],
+    );
   }
 
   @override
