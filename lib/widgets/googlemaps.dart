894de05f270f6a89a3fb129bc6788f4b646e3db8
sarahannali <55365172+sarahannali@users.noreply.github.com>
2020-07-03 18:31:52 -0500

    [Gor 28] Show JSON Objects on Map (#4)
    * added starbucks.json data
    
    * add updated pubspec
    
    * add display of starbuck location markers
    
    * make connection string const
    
    * remove excess parameter from getDBLocations
    
    * add newline to json file
    
    * remove excess print statements
    
    * update nearbyplaces file
    
    * remove extra position var in google maps
    
    * make getNearbyLocations a static method
    
    * update googlemaps imports
    
    * moving member variables in locations.dart
    
    * removed excess linting comment
    
    * moved dependencies

--- lib/widgets/googlemaps.dart
+++ lib/widgets/googlemaps.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../data/userlocation.dart';
-import '../screens/loading.dart';
+import 'package:gorilla_eats/data/locations.dart';
+import 'package:gorilla_eats/data/userlocation.dart';
+import 'package:gorilla_eats/screens/loading.dart';
 
 const double zoomLevel = 15;
 
@@ -24,12 +25,31 @@ class _GoogleMapsState extends State<GoogleMaps> {
     });
   }
 
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final nearbyLocations = await Location.getNearbyLocations();
+    setState(() {
+      _markers.clear();
+      for (final location in nearbyLocations) {
+        final marker = Marker(
+          markerId: MarkerId(location.address),
+          position: LatLng(location.lat, location.lng),
+          infoWindow: InfoWindow(
+            title: location.name,
+            snippet: location.address,
+          ),
+        );
+        _markers[location.address] = marker;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _initialPosition == null
           ? LoadingScreen()
           : GoogleMap(
+              onMapCreated: _onMapCreated,
               myLocationEnabled: true,
               initialCameraPosition: CameraPosition(
                 target: _initialPosition,
