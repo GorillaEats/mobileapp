f560d8ca8e14c8dc4db8b4a59205ec76101183a5
Sarah Ali <asarahali00@gmail.com>
2020-07-03 19:14:00 -0500

    add map cards to map screen

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:gorilla_eats/widgets/mapcards.dart';
 import '../widgets/googlemaps.dart';
 
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMaps(),
+      body: Stack(
+        children: <Widget>[
+          GoogleMaps(),
+          MapCards(),
+        ],
+      ),
     );
   }
 }
