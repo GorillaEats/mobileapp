584bf21e9f9d3341211d46393a605c0541aacb19
Sarah <asarahali00@gmail.com>
2020-08-07 14:19:18 -0500

    remove _listView from search model

--- lib/data/models/search.dart
+++ lib/data/models/search.dart
@@ -9,7 +9,7 @@ import 'package:gorilla_eats/credentials.dart';
 import 'package:gorilla_eats/data/locations.dart' as gorilla_location;
 
 const releaseBaseUrl = 'www.gorillaeats.com';
-const debugBaseUrl = '192.168.0.10:8080';
+const debugBaseUrl = '192.168.1.221:8080';
 const locationsPath = '/locations';
 const maxSearchDistanceMeters = 20.0 * 1609.0;
 const double zoomLevel = 13;
@@ -119,7 +119,6 @@ class SearchModel extends ChangeNotifier {
     }
 
     var response = await http.get(uri);
-
     if (response.statusCode == 200) {
       dynamic bodyJson = json.decode(response.body);
       var locationsJson = bodyJson['locations'] as List<dynamic>;
