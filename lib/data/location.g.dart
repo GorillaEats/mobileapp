1732e5bf969449d26b354247203bb6d12f3c765a
Kevo1ution <kevin26428@gmail.com>
2020-08-08 12:08:20 -0500

    renamed location.dart to singular

--- lib/data/location.g.dart
+++ lib/data/location.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'locations.dart';
+part of 'location.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
