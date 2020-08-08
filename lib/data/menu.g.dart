a6336d72e2e301a9f316eb42763452e894270d4a
Kevo1ution <kevin26428@gmail.com>
2020-08-08 11:19:22 -0500

    combined items into menu file

--- lib/data/menu.g.dart
+++ lib/data/menu.g.dart
@@ -6,6 +6,23 @@ part of 'menu.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+Item _$ItemFromJson(Map<String, dynamic> json) {
+  return Item(
+    name: json['name'] as String,
+    category: json['category'] as String,
+    modifications:
+        (json['modifications'] as List)?.map((e) => e as String)?.toList(),
+    tags: (json['tags'] as List)?.map((e) => e as String)?.toList(),
+  );
+}
+
+Map<String, dynamic> _$ItemToJson(Item instance) => <String, dynamic>{
+      'name': instance.name,
+      'category': instance.category,
+      'modifications': instance.modifications,
+      'tags': instance.tags,
+    };
+
 Menu _$MenuFromJson(Map<String, dynamic> json) {
   $checkKeys(json, requiredKeys: const ['items', 'source', 'restaurantId']);
   return Menu(
