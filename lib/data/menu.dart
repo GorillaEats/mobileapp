63561727da083710c78ea91df28dddbfc45a1b73
Kevo1ution <kevin26428@gmail.com>
2020-08-08 11:19:22 -0500

    combined items into menu file

--- lib/data/menu.dart
+++ lib/data/menu.dart
@@ -4,6 +4,20 @@ import 'package:gorilla_eats/data/item.dart';
 part 'menu.g.dart';
 
 @JsonSerializable()
+class Item{
+  String name;
+  String category;
+  List<String> modifications;
+  List<String> tags;
+
+  Item({this.name, this.category, this.modifications, this.tags});
+
+  factory Item.fromJson(Map<String, dynamic> json) => _$ItemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ItemToJson(this);
+}
+
+@JsonSerializable()
 class Menu {
   @JsonKey(required: true)
   List<Item> items;
